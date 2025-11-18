0e42a98d4b1e894e3508937005869b70f4307cde
Timmeyyyy <timekawin.p@gmail.com>
2025-11-18 13:52:55 +0700

    Kawin Phonruk

---
+++ Lab101.java
@@ -0,0 +1,8 @@
+public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Kawin Phonruk" +
+                //"\n\"INT-108\" 'Object-Oreint Programming'" +
+                "\n\tThis is my first time learning Java" +
+                "\n\tI hope to get grade \"a\"");
+    }
+}
